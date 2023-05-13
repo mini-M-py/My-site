f128f9976c669f4e91d87210d398d6cb8209f1d9
Manish Dhakal <dhakalm909@gmail.com>
2023-05-13 18:51:05 +0545

    create post

--- script.js
+++ script.js
@@ -5,55 +5,92 @@ let timerId;
 
 
 function showNotificationWithDelay(messageText, delay) {
-  setTimeout(() => {
-    showNotification(messageText);
-  }, delay);
+    setTimeout(() => {
+        showNotification(messageText);
+    }, delay);
 }
 
 
 function showNotification(messageText) {
-  message.textContent = messageText;
-  notification.classList.add('active');
-  timerId = setTimeout(() => {
-    hideNotification();
-  }, 15000);
+    message.textContent = messageText;
+    notification.classList.add('active');
+    timerId = setTimeout(() => {
+        hideNotification();
+    }, 15000);
 }
 
 
 function hideNotification() {
-  notification.classList.remove('active');
-  clearTimeout(timerId);
+    notification.classList.remove('active');
+    clearTimeout(timerId);
 }
 
 
 showNotificationWithDelay('Budget is low, server is slow, it will take time to load', 2000);
 
-function login(event) {
-    
-    let formEl = document.querySelector('.form');
-
-    formEl.addEventListener('submit', event => {
-        event.preventDefault();
-
-        let formData = new FormData(formEl);
-        let data = new URLSearchParams(formData);
-
-        fetch('https://mini-media.onrender.com/login', {
-            method: 'POST',
-            body: data,
-        }).then(res => res.json())
-            .then(data => {
-                if (data.access_token) {
-                    localStorage.setItem('token', data.access_token);
-                    window.location.href = 'index.html';
-                } else {
-                    showNotificationWithDelay('Invalid eamil and password. Try again!', 1000);
-                }
-            })
-            .catch(error => console.log(error));
-    })
+//submit Posts
+//const //popupBackground = document.getElementById("popupBackground")
+function showPopup() {
+    document.getElementById("popupBackground").style.display = "block";
 }
 
+const createPost = document.querySelector("#submitPost")
+
+function submit(token) {
+    var title = document.getElementById("postTitle").value;
+    var content = document.getElementById("postContent").value;
+    var privacy = document.getElementById("postPrivacy").value;
+
+    // Perform any necessary validation on the form inputs
+    var published = privacy === "public";
+    // Create a new post object or send the data to the server
+    var post = {
+        title: title,
+        content: content,
+        published: published
+    };
+    console.log(post)
+
+    // Perform any necessary AJAX request to submit the post data
+    // Example using Fetch API
+    fetch("https://mini-media.onrender.com/posts/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+        },
+        body: JSON.stringify(post)
+    })
+        .then(response => {
+            if (response.ok) {
+                // Post submitted successfully
+                console.log("Post submitted successfully");
+                // Reset the form inputs
+                document.getElementById("postTitle").value = "";
+                document.getElementById("postContent").value = "";
+                document.getElementById("postPrivacy").value = "public";
+                document.getElementById("popupBackground").style.display = "none";
+
+            } else {
+                // Post submission failed
+                showNotificationWithDelay("Something Went Wrong, Try again!!!", 1000)
+            }
+        })
+        .catch(error => {
+            // Error occurred during the request
+            console.error("An error occurred during the post submission:", error);
+        });
+}
+
+function cancel() {
+    // Reset the form inputs
+    document.getElementById("postTitle").value = "";
+    document.getElementById("postContent").value = "";
+    document.getElementById("postPrivacy").value = "public";
+    document.getElementById("popupBackground").style.display = "none";
+}
+
+//home
 function home(token) {
     fetch('https://mini-media.onrender.com/posts/', {
         headers: {
@@ -83,23 +120,24 @@ function home(token) {
         .catch(error => console.log(error));
 }
 
+
 function registered(event) {
     const formEl = document.querySelector('.form1');
 
 
     formEl.addEventListener('submit', event => {
         event.preventDefault();
-         if (!otpSent) {
+        if (!otpSent) {
             showNotificationWithDelay("Please send OTP before registering.", 1000);
             return;
-         }
+        }
         const user = {
             user_name: document.querySelector('input[name="user_name"]').value,
             email: document.querySelector('input[name="email"]').value,
             password: document.querySelector('input[name="password"]').value,
             otp: document.querySelector('input[name="otp"]').value
         }
-       
+
 
         fetch('https://mini-media.onrender.com/user', {
             method: 'POST',
@@ -127,7 +165,31 @@ function registered(event) {
     })
 }
 
+function login(event) {
 
+    let formEl = document.querySelector('.form');
+
+    formEl.addEventListener('submit', event => {
+        event.preventDefault();
+
+        let formData = new FormData(formEl);
+        let data = new URLSearchParams(formData);
+
+        fetch('https://mini-media.onrender.com/login', {
+            method: 'POST',
+            body: data,
+        }).then(res => res.json())
+            .then(data => {
+                if (data.access_token) {
+                    localStorage.setItem('token', data.access_token);
+                    window.location.href = 'index.html';
+                } else {
+                    showNotificationWithDelay('Invalid eamil and password. Try again!', 1000);
+                }
+            })
+            .catch(error => console.log(error));
+    })
+}
 
 const formEl = document.querySelector('.form1');
 const sendBtn = document.querySelector("#send");
@@ -135,43 +197,44 @@ const otpText = document.querySelector("#otp_text");
 let otpSent = false;
 
 function otp(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const user = {
-    email: document.querySelector('input[name="email"]').value,
-  };
+    const user = {
+        email: document.querySelector('input[name="email"]').value,
+    };
 
-  fetch('https://mini-media.onrender.com/user/verified', {
-    method: 'POST',
-    headers: {
-      'Content-Type':'application/json'
-    },
-    body: JSON.stringify(user)
-  }).then(res => {
-    if (res.status === 500) {
-        showNotificationWithDelay("Couldn't send email. Try again!!!", 1000);
-    }
-    else if (res.status === 422){
-        showNotificationWithDelay("invalid credential", 1000);
-    }
-    return res.json();
-  }).then(data => {
-    otpSent = true;
-    sendBtn.innerText = "Sent";
-    otpText.innerText = "Check your email for the OTP.";
-  }).catch(error => console.log(error))
+    fetch('https://mini-media.onrender.com/user/verified', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    }).then(res => {
+        if (res.status === 500) {
+            showNotificationWithDelay("Couldn't send email. Try again!!!", 1000);
+        }
+        else if (res.status === 422) {
+            showNotificationWithDelay("invalid credential", 1000);
+        }
+        return res.json();
+    }).then(data => {
+        otpSent = true;
+        sendBtn.innerText = "Sent";
+        otpText.innerText = "Check your email for the OTP.";
+    }).catch(error => console.log(error))
 }
 
 function register(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  if (!otpSent) {
-    showNotificationWithDelay("Please send OTP before registering.", 1000);
-    return;
-  }
+    if (!otpSent) {
+        showNotificationWithDelay("Please send OTP before registering.", 1000);
+        return;
+    }
 
 
 }
 
 sendBtn.addEventListener('click', otp);
 formEl.addEventListener('submit', register);
+//createPost.addEventListener('click', submit(token));
